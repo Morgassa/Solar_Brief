525394c37388648e5490a6a4f6c236f716a4a929
Morgassa <amadeudeassuncao@gmail.com>
2020-02-14 17:34:08 -0300

    XD

--- Rascunho.py
+++ Rascunho.py
@@ -1,9 +1,11 @@
 # http://www.cresesb.cepel.br/index.php?section=sundata&
 
-Acessar, preecher e pegar os dados da localização.
+# Acessar, preecher e pegar os dados da localização.
 
 from selenium import webdriver
 
 driver = webdriver.Chrome('./chromedriver')
-driver.maximize_window()
+#driver.maximize_window()
+driver.minimize_window()
+
 driver.get("http://www.cresesb.cepel.br/index.php?section=sundata&")
