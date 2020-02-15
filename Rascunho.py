4d2109c6cf8b52679aa7fa4a5e2c936ef6e1ca65
Morgassa <amadeudeassuncao@gmail.com>
2020-02-15 19:46:44 -0300

    XD

--- Rascunho.py
+++ Rascunho.py
@@ -1,6 +1,5 @@
 # http://www.cresesb.cepel.br/index.php?section=sundata&
 
-# Acessar, preecher e pegar os dados da localização.
 
 Latitude = '8.0113981'
 Longitude = '34.8734516'
@@ -16,30 +15,27 @@ import time
 
 class solar_gather_bot():
     
-    def __init__(self, lat, long):
+    def __init__(self):
         self.driver = webdriver.Chrome('./chromedriver')
-        self.latitude = lat
-        self.longitude = long
+        print('Inicializado...')
 
 
     def acess_site():
-        go = self.driver.get("http://www.cresesb.cepel.br/index.php?section=sundata&")
+        self.driver.get("http://www.cresesb.cepel.br/index.php?section=sundata&")
 
         assert "Python running..." in self.driver.title
 
 
-    def put_coords():
+    def put_coords(self):
         lat_in = self.driver.find_element_by_xpath('//*[@id="latitude_dec"]')
-        lat_in.send_keys(self.latitude)
+        lat_in.send_keys(8.666)
 
         long_in = self.driver.find_element_by_xpath('//*[@id="longitude_dec"]')
-        long_in.send_keys(self.longitude)
+        long_in.send_keys(50.666)
 
         send_button = self.driver.find_element_by_xpath('//*[@id="submit_btn"]')
         send_button.click()
 
     
     def get_gata():
-        stations_data = self.driver.find_element_by_xpath('//*[@id="tb_sundata"]/tbody')
-        # angulos_data = 
-        # df = pd.read_html(stations_data)
+        self.put_coords()
--- graphics.py
+++ graphics.py
@@ -1,10 +1,9 @@
 from Rascunho import solar_gather_bot
 
 
-bot = solar_gather_bot
+bot = solar_gather_bot()
 
-bot.acess_site
-
-
+bot.driver.get("http://www.cresesb.cepel.br/index.php?section=sundata&")
+bot.put_coords()
 
 
