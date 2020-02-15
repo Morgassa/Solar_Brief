7c5ba45e665cb1f67b0b5cdccd2b5956b92d8b19
Morgassa <amadeudeassuncao@gmail.com>
2020-02-14 21:19:32 -0300

    XD

--- Rascunho.py
+++ Rascunho.py
@@ -1,13 +1,37 @@
 # http://www.cresesb.cepel.br/index.php?section=sundata&
 
 # Acessar, preecher e pegar os dados da localização.
-# Latitude = 8.0113981
-# Longitude = 34.8734516
+Latitude = 8.0113981
+Longitude = 34.8734516
 
 from selenium import webdriver
 
-driver = webdriver.Chrome('./chromedriver')
-#driver.maximize_window()
-driver.minimize_window()
+class solar_gather_bot():
+    
+    def __init__(self):
+        self.driver = webdriver.Chrome('./chromedriver')
 
-driver.get("http://www.cresesb.cepel.br/index.php?section=sundata&")
+    def acess_site():
+        self.driver.get("http://www.cresesb.cepel.br/index.php?section=sundata&")
+
+        lat_in = self.driver.find_element_by_xpath('//*[@id="latitude_dec"]')
+        lat_in.send_keys(Latitude)
+
+        long_in = self.driver.find_element_by_xpath('//*[@id="longitude_dec"]')
+        long_in.send_keys(Latitude)
+
+
+        send_button = self.driver.find_element_by_xpath('//*[@id="submit_btn"]')
+        send_button.click()
+
+    def get_sun_data():
+        
+
+
+    #driver.maximize_window()
+    #driver.minimize_window()
+
+    
+
+    #lat_in = driver.find_elemnt_by_xpath('^[[200~//*[@id="latitude_dec"]~')
+    #lat_in.send_keys('amadeu@')
