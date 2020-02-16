bc2a73a51491a211d3e88fe5c48bb26bdd3afa10
Morgassa <amadeudeassuncao@gmail.com>
2020-02-16 10:59:06 -0300

    XD

--- graphics.py
+++ graphics.py
@@ -2,13 +2,13 @@ from Rascunho import solar_gather_bot
 
 
 Latitude = '8.0113981'
-Longitude = '34.8734516'
+Longitude = '38.8734516'
 
 
 bot = solar_gather_bot()
 
 bot.driver.get("http://www.cresesb.cepel.br/index.php?section=sundata&")
 bot.put_coords()
-bot.get_gata()
+# print(bot.get_gata())
+print(bot.create_database())
 
-head = ['Estação', 'Distância', 'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez', 'Média', 'Delta']
